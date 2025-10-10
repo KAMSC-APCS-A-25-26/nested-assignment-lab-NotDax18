2fee0fd88de91184d357851574704d05ef9340a7
NotDax18 <dax.cw.robinson@gmail.com>
2025-10-10 13:32:04 -0400

    Update LeapYearChecker.java

--- src/main/java/LeapYearChecker.java
+++ src/main/java/LeapYearChecker.java
@@ -2,10 +2,25 @@ import java.util.Scanner;
 
 public class LeapYearChecker {
     public static void main(String[] args) {
-        // TODO: Step 1 - Get input from user
+        Scanner scanner = new Scanner(System.in);
 
-        // TODO: Step 2 - Implement nested if logic for leap year
+        System.out.print("Enter a year: ");
+        int year = scanner.nextInt();
 
-        // TODO: Step 3 - Display the result
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    System.out.println(year + " is a leap year.");
+                } else {
+                    System.out.println(year + " is NOT a leap year.");
+                }
+            } else {
+                System.out.println(year + " is a leap year.");
+            }
+        } else {
+            System.out.println(year + " is NOT a leap year.");
+        }
+
+        scanner.close();
     }
 }
