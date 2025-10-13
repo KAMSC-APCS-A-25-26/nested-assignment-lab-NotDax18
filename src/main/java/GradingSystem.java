088327b3cad6fdba267d55d0d23a74f60e2b8bd4
NotDax18 <dax.cw.robinson@gmail.com>
2025-10-13 13:47:48 -0400

    Update GradingSystem.java

--- src/main/java/GradingSystem.java
+++ src/main/java/GradingSystem.java
@@ -2,58 +2,68 @@ import java.util.Scanner;
 
 public class GradingSystem {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+
+        // TODO: Step 1 - Get the score from user
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a score (0-100):  ");
+        int score = sc.nextInt();
         String grade = "";
-        String modifier = "";
-        int score;
-        System.out.println("Please enter a score between 0 and 100: ");
-        score = scan.nextInt();
-        if (score < 60) {
-            grade = "F";
+
+        if(score < 0 || score > 100){
+            grade = "Invalid score";
         }
+        else{
+            if(score >= 90){
+                grade = "A";
+                if(score >= 98){
+                    grade += "+";
+                }
+                if(score <=93){
+                    grade += "-";
+                }
+            }
+            else if(score >= 80){
+                grade = "B";
+                if(score >= 88){
+                    grade += "+";
+                }
+                if(score <=83){
+                    grade += "-";
+                }
+            }
+            else if(score >= 70){
+                grade = "C";
+                if(score >= 78){
+                    grade += "+";
+                }
+                if(score <=73){
+                    grade += "-";
+                }
+            }
+            else if(score >= 60){
+                grade = "D";
+                if(score >= 68){
+                    grade += "+";
+                }
+                if(score <=63){
+                    grade += "-";
+                }
+            }
+            else{
+                grade = "F";
+            }
 
-
-
-        if (score >= 90) {
-            grade = "A";
-            if (score >= 98) {
-                modifier = "+";
-            } else if (score >= 94) {
-                modifier = "";
-            } else {
-                modifier = "-";
-            }
-        } else if (score >= 80) {
-            grade = "B";
-            if (score >= 88) {
-                modifier = "+";
-            } else if (score >= 84) {
-                modifier = "";
-            } else {
-                modifier = "-";
-            }
-        } else if (score >= 70) {
-            grade = "C";
-            if (score >= 78) {
-                modifier = "+";
-            } else if (score >= 74) {
-                modifier = "";
-            } else {
-                modifier = "-";
-            }
-        } else if (score >= 60) {
-            grade = "D";
-            if (score >= 68) {
-                modifier = "+";
-            } else if (score >= 64) {
-                modifier = "";
-            } else {
-                modifier = "-";
-            }
         }
-        System.out.println("The grade is: " + grade+modifier);
+        System.out.println("The grade is: " + grade);
+
+
+
+
+
+        // TODO: Step 2 - Implement nested if logic for letter grades
+
+
+        // TODO: Step 3 - Display the result
+
     }
-
-
 }
-
